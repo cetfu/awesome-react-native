762960c5d21e1c68697d799fc3ae14a64888cc59
Furkan Cetinkaya <furkancetinkaya0746@gmail.com>
2024-01-10 15:20:37 +0300

    configured development packages

--- template/babel.config.js
+++ template/babel.config.js
@@ -2,5 +2,15 @@ module.exports = {
     presets: ['module:@react-native/babel-preset'],
     plugins: [
         'react-native-reanimated/plugin',
+        [
+            require.resolve('babel-plugin-module-resolver'),
+            {
+                cwd: 'babelrc',
+                extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+                alias: {
+                    '@': './src',
+                },
+            },
+        ],
     ],
 };
