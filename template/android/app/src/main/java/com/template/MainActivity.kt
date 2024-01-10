9a699fc1c5479c868911550e6d2d97f1d57e6a95
Furkan Cetinkaya <furkancetinkaya0746@gmail.com>
2024-01-10 15:33:07 +0300

    rename references

--- template/android/app/src/main/java/com/template/MainActivity.kt
+++ template/android/app/src/main/java/com/template/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Template"
+  override fun getMainComponentName(): String = "template"
 
   override fun onCreate(savedInstanceState: Bundle?) {
      super.onCreate(null)
